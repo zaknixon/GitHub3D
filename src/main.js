437a944e990ae447aef88f1073b1f8e3f07dc440
Zak Nixon <znixon@gmail.com>
2023-12-19 07:46:54 -0600

    Imported Three.Meshline for our scene.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline';
 
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
+// const controls = new OrbitControls( camera, renderer.domElement );
+// const loader = new GLTFLoader();
